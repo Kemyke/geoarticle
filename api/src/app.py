03fd5797999d5947cc18a2b727ff8218d01f14a0
Kemy <kemyyy@gmail.com>
2019-10-16 15:45:42 +0000

    locationiq api

--- api/src/app.py
+++ api/src/app.py
@@ -8,9 +8,27 @@ import numpy
 import requests
 import json
 import os
+import time
 
 app = Flask(__name__)
 CORS(app)
+
+def nexogen_geocode(city):
+    geocodeurl = os.environ.get('GEOARTICLE_GEOCODE_URL') + '/gis/v1/geocode?Address=' + city + '&Limit=1&Structured=false&Priority=Low'
+    response = requests.get(geocodeurl, headers={'Authorization': 'Bearer ' + os.environ.get('GEOARTICLE_GEOCODE_APIKEY')})
+    results = json.loads(response.text)
+    if len(results['results']) > 0:
+        return [ results['results'][0]['centerPoint']['latitude'], results['results'][0]['centerPoint']['longitude'] ]
+    return []
+
+def locationiq_geocode(city):
+    geocodeurl = os.environ.get('GEOARTICLE_GEOCODE_URL') + '/v1/search.php?key=' + os.environ.get('GEOARTICLE_GEOCODE_APIKEY') + '&q=' + city + '&format=json'
+    response = requests.get(geocodeurl)
+    results = json.loads(response.text)
+    time.sleep(0.5)
+    if len(results) > 0:
+        return [ results[0]['lat'], results[0]['lon'] ]
+    return []
 
 @app.route('/geo/<articleurl>')
 def geo(articleurl):
@@ -22,10 +40,10 @@ def geo(articleurl):
     cities = numpy.unique(places.cities).tolist()
 
     ret = {}
+    ret['cities']={}
     for city in cities:
-        geocodeurl = os.environ.get('GEOARTICLE_GEOCODE_URL') + '/gis/v1/geocode?Address=' + city + '&Limit=1&Structured=false&Priority=Low'
-        response = requests.get(geocodeurl, headers={'Authorization': 'Bearer ' + os.environ.get('GEOARTICLE_GEOCODE_APIKEY')})
-        results = json.loads(response.text)
-        ret[city] = [ results['results'][0]['centerPoint']['latitude'], results['results'][0]['centerPoint']['longitude'] ]
-
+        coord = locationiq_geocode(city)
+        if len(coord) > 0:
+            ret['cities'][city] = coord
+    ret['article']=article.text
     return jsonify(ret=ret) 
